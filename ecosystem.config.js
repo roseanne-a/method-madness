eb342d4e252affc0866652450b064f0df2f259c5
roseanne-a <roseanne.arcebido@gmail.com>
2023-08-08 22:22:30 -0400

    change script for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "app",
-      script: "./index.js",
+      script: "index.js",
       instances: "max",
       env: {
         NODE_ENV: "development",
